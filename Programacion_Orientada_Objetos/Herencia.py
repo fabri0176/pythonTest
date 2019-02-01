94e8e6be730ba9e6d1bdaa53ecd020cc20100f4a
Nestor F Parra <nparra@existaya.com>
2019-01-31 21:23:17 -0500

    Herencia

---
+++ Programacion_Orientada_Objetos/Herencia.py
@@ -0,0 +1,60 @@
+class Producto:
+    def __init__(self,referencia, nombre,pvp,descripcion):
+        self.referencia = referencia
+        self.nombre = nombre
+        self.pvp = pvp
+        self.descripcion = descripcion
+
+    def __str__(self):
+        return """\
+REFERENCIA\t{}
+NOMBRE\t\t{}
+PVP\t\t\t{}
+DESCRIPCIÓN\t{}""".format(self.referencia,self.nombre,self.pvp,self.descripcion)
+
+class Adorno(Producto):
+    pass
+
+class Alimento(Producto):
+    productor = ""
+    distribuidor = ""
+
+    def __str__(self):
+        return """\
+REFERENCIA\t\t{}
+NOMBRE\t\t\t{}
+PVP\t\t\t\t{}
+DESCRIPCIÓN\t\t{}
+PRODUCTOR\t\t{}
+DISTRIBUIDOR\t{}""".format(self.referencia, self.nombre, self.pvp, self.descripcion,self.productor,self.distribuidor)
+
+class Libro(Producto):
+    isbn = ""
+    autor = ""
+
+    def __str__(self):
+        return """\
+REFERENCIA\t\t{}
+NOMBRE\t\t\t{}
+PVP\t\t\t\t{}
+DESCRIPCIÓN\t\t{}
+ISBN\t\t\t{}
+AUTOR\t\t\t{}""".format(self.referencia, self.nombre, self.pvp,self.descripcion, self.isbn, self.autor)
+
+
+a = Adorno(2034,"Vaso adornado",15,"Vaso de porcelana adornado con árboles")
+print(a)
+
+a1 = Alimento(2034,"Botella Aceita Extra",5,"250 ML")
+a1.productor = "La Aceitera"
+a1.distribuidor = "Distribuiciones SA"
+print(a1)
+
+
+l1 = Libro(2036,"El carnal",6,"Libro el carnal")
+l1.autor = "Juan Cobo"
+l1.isbn = "44455259"
+print(l1)
+
+
+
