0f589517c7c66bb1d8ed1f9f29baade5c7cec14b
Nestor Fabricio Parra <fabri0176@gmail.com>
2019-02-02 20:32:06 -0500

    Modulos

---
+++ Modulos/saludos.py
@@ -0,0 +1,7 @@
+# Este es un modulo con funciones que saludan
+def saludar():
+    print("Te estoy saludando desde la función saludar del modulo saludos")
+
+class Saludo():
+    def __init__(self):
+        print("Hola estoy salundando desde el init de la clase saludo")
